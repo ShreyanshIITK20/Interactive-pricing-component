c67cf9c8bc415ce0e161d8960b3c11ce69238b8b
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-09-17 15:55:06 +0530

    Added all toggling features

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ var payment = {
     price:""
 }
 
+var checked = false
+
 function netVal (value){
     if(value==1){
         payment.pageview="10K"
-        payment.price="$8.00"
+        payment.price=8.00.toFixed(2)
     } else if(value==2){
         payment.pageview="50K"
-        payment.price="$12.00"
+        payment.price=12.00.toFixed(2)
     } else if(value==3){
         payment.pageview="100K"
-        payment.price="$16.00"
+        payment.price=16.00.toFixed(2)
     } else if(value==4){
         payment.pageview="500K"
-        payment.price="$24.00"
+        payment.price=24.00.toFixed(2)
     } else if(value==5){
         payment.pageview="1M"
-        payment.price="$36.00"
+        payment.price=36.00.toFixed(2)
     }
+    
+    if(checked) payment.price*=0.75
     
     return payment;
 }
@@ -29,4 +33,10 @@ function slideFunction(val){
     $('.pricing').html(netVal(val).price)
 }
 
-console.log('#flexSwitchCheckDefault')
+function toggle(){
+    checked = !checked
+    if(checked) $('.pricing').html((($('.pricing').html())*0.75).toFixed(2))
+    else $('.pricing').html((($('.pricing').html())*(4/3)).toFixed(2))
+}
+
+
